402ae501564a564f17ffa0334be07b303ea9ff40
rahul <rahulkaz28@gmail.com>
2025-09-25 09:14:52 +0530

    evenOdd added

---
+++ src/ex_01_Java_Basics/Ternary_EvenOdd.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basics;
+
+public class Ternary_EvenOdd {
+    public static void main(String[] args) {
+        int num=32;
+
+        String result = (num%2==0) ? "Even" : "odd";
+        System.out.println(result);
+
+    }
+}
